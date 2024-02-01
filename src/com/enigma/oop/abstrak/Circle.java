163b74ccddef6adfb863922ba6db297b32e69c6c
adamfachreza <adamfachreza15@gmail.com>
2024-02-02 02:14:09 +0700

    equals dan hashset

--- src/com/enigma/oop/abstrak/Circle.java
+++ src/com/enigma/oop/abstrak/Circle.java
@@ -1,10 +1,37 @@
 package com.enigma.oop.abstrak;
 
+import java.util.Objects;
+
 public class Circle extends Shape{
+
+    private Integer radius;
+
+    public Circle(Integer radius) {
+        this.radius = radius;
+    }
+
     @Override
     void draw() {
         System.out.println("Drawing a Circle");
     }
+// equals untuk ngebandingin valuenya, kalo g make equals yang di bandingin itu alamat memorynya
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return Objects.equals(radius, circle.radius);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
 
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
 }
--- src/com/enigma/oop/abstrak/Main.java
+++ src/com/enigma/oop/abstrak/Main.java
@@ -1,13 +1,29 @@
 package com.enigma.oop.abstrak;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle();
-        circle.draw();
-        circle.display();
+//        Circle circle = new Circle(2);
+//        System.out.println(circle.equals(new Circle(2)));
 
-        Square square = new Square();
-        square.draw();
-        square.display();
+//        circle.draw();
+//        circle.display();
+
+//        Square square = new Square();
+//        square.draw();
+//        square.display();
+        Set<Circle> circleSet = new HashSet<>();
+        circleSet.add(new Circle(10));
+        circleSet.add(new Circle(30));
+        circleSet.add(new Circle(20));
+        circleSet.add(new Circle(1));
+        circleSet.add(new Circle(10));
+        for(Circle circle : circleSet){
+            System.out.println(circle.toString());
+        }
+        System.out.println("Size : " + circleSet.size());
+
     }
 }
