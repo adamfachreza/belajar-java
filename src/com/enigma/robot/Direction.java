4adde0814934fdc5abcf5f99765ebd14d33cf46d
adamfachreza <adamfachreza15@gmail.com>
2024-02-02 01:23:55 +0700

    refactoring robot

--- src/com/enigma/robot/Direction.java
+++ src/com/enigma/robot/Direction.java
@@ -3,11 +3,13 @@ package com.enigma.robot;
 public enum Direction {
     NORTH,EAST,SOUTH,WEST;
 
+    // ordinal urutan index di enum [NORT =0 ,EAST = 1,SOUTH = 2,WEST = 3]
     public Direction turnRight(){
-        return Direction.values()[(this.ordinal()+1%4)];
+        return Direction.values()[(this.ordinal()+1)%4];
+
     }
     public Direction turnLeft(){
-        return Direction.values()[this.ordinal()+3%4];
+        return Direction.values()[(this.ordinal()+3)%4];
     }
 
     }
